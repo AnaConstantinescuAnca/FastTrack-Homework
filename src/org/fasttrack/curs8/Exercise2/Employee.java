54046571056280f187252ae9585e598f7f5a68b3
ddcmpower <ddcmpower@gmail.com>
2022-09-19 16:16:53 +0300

    Exercise2- curs8 -second commit

--- src/org/fasttrack/curs8/Exercise2/Employee.java
+++ src/org/fasttrack/curs8/Exercise2/Employee.java
@@ -1,4 +1,47 @@
 package org.fasttrack.curs8.Exercise2;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
 
-public class Employee {
+public class Employee implements Person{
+    private String firstName;
+    private String lastName;
+    private LocalDate birthDay;
+    private String address;
+    private LocalDate dateOfEmployment;
+    private String position;
+
+    public Employee(String firstName, String lastName, LocalDate birthDay, String address, LocalDate dateOfEmployement, String position) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDay = birthDay;
+        this.address = address;
+        this.dateOfEmployment = dateOfEmployment;
+        this.position = position;
+    }
+
+    @Override
+    public String getFirstName() {
+        return firstName;
+    }
+
+    @Override
+    public String getLastName() {
+        return lastName;
+    }
+
+    @Override
+    public LocalDate getBirthDay() {
+        return birthDay;
+    }
+
+    @Override
+    public String getAddress() {
+        return address;
+    }
+
+    public String  calculVechime(){
+        Period vechime = Period.between(this.dateOfEmployment, LocalDate.now());
+        return String.format("Ani %s, Luni %s, zile %s",vechime.getYears(),vechime.getMonths(),vechime.getDays());
+    }
 }
---
+++ src/org/fasttrack/curs8/Exercise2/Person.java
@@ -0,0 +1,10 @@
+package org.fasttrack.curs8.Exercise2;
+import java.time.LocalDate;
+import java.util.Date;
+
+public interface Person {
+    String getFirstName();
+    String getLastName();
+    LocalDate getBirthDay();
+    String getAddress();
+}
